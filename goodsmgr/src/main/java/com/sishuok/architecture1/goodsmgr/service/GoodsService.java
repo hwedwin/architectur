3424f80f22bbc50057ba116cd7f895a042b1fb61
jackznq <1577237702@qq.com>
2017-07-27 10:38:47 +0800

    库存bug修改

--- goodsmgr/src/main/java/com/sishuok/architecture1/goodsmgr/service/GoodsService.java
+++ goodsmgr/src/main/java/com/sishuok/architecture1/goodsmgr/service/GoodsService.java
@@ -1,15 +1,13 @@
 package com.sishuok.architecture1.goodsmgr.service;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.sishuok.architecture1.common.service.BaseService;
 import com.sishuok.architecture1.goodsmgr.dao.GoodsDAO;
 import com.sishuok.architecture1.goodsmgr.vo.GoodsModel;
 import com.sishuok.architecture1.goodsmgr.vo.GoodsQueryModel;
-import com.sishuok.pageutil.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
--- ordermgr/src/main/java/com/sishuok/architecture1/ordermgr/service/OrderService.java
+++ ordermgr/src/main/java/com/sishuok/architecture1/ordermgr/service/OrderService.java
@@ -1,19 +1,24 @@
 package com.sishuok.architecture1.ordermgr.service;
 
 
-import java.util.Date;
-
+import com.sishuok.architecture1.cartmgr.service.ICartService;
+import com.sishuok.architecture1.cartmgr.vo.CartModel;
+import com.sishuok.architecture1.cartmgr.vo.CartQueryModel;
+import com.sishuok.architecture1.common.service.BaseService;
+import com.sishuok.architecture1.ordermgr.dao.OrderDAO;
+import com.sishuok.architecture1.ordermgr.queue.QueueSender;
+import com.sishuok.architecture1.ordermgr.vo.OrderDetailModel;
+import com.sishuok.architecture1.ordermgr.vo.OrderModel;
+import com.sishuok.architecture1.ordermgr.vo.OrderQueryModel;
+import com.sishuok.architecture1.storemgr.service.IStoreService;
+import com.sishuok.architecture1.storemgr.vo.StoreModel;
+import com.sishuok.pageutil.Page;
+import com.sishuok.util.format.DateFormatHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.sishuok.architecture1.cartmgr.service.ICartService;
-import com.sishuok.architecture1.common.service.BaseService;
-import com.sishuok.architecture1.ordermgr.dao.OrderDAO;
-import com.sishuok.architecture1.ordermgr.queue.QueueSender;
-import com.sishuok.architecture1.ordermgr.vo.OrderModel;
-import com.sishuok.architecture1.ordermgr.vo.OrderQueryModel;
-import com.sishuok.architecture1.storemgr.service.IStoreService;
+import java.util.Date;
 
 @Service
 @Transactional
@@ -37,55 +42,55 @@ public class OrderService extends BaseService<OrderModel,OrderQueryModel> implem
 		}
 		
 		
-//			//消息的消费者来完成如下功能
-//			CartQueryModel cqm = new CartQueryModel();
-//			cqm.getPage().setPageShow(1000);
-//			cqm.setCustomerUuid(customerUuid);
-//			
-//			Page<CartModel>  page = ics.getByConditionPage(cqm);
-//			//2:
-//			float totalMoney = 0.0f;
-//			for(CartModel cm : page.getResult()){
-//				totalMoney += 10;
-//			}
-//			
-//			OrderModel order = new OrderModel();
-//			order.setCustomerUuid(customerUuid);
-//			order.setOrderTime(DateFormatHelper.long2str(System.currentTimeMillis()));
-//			order.setSaveMoney(0.0F);
-//			order.setTotalMoney(totalMoney);
-//			order.setState(1);
-//	
-//			create(order);
-//			
-//			OrderQueryModel oqm = new OrderQueryModel();
-//			oqm.setOrderTime(order.getOrderTime());
-//			
-//			Page<OrderModel>  orderPage = getByConditionPage(oqm);
-//			order = orderPage.getResult().get(0);
-//			
-//			//3:
-//			for(CartModel cm : page.getResult()){
-//				OrderDetailModel odm = new OrderDetailModel();
-//				odm.setGoodsUuid(cm.getGoodsUuid());
-//				odm.setOrderNum(cm.getBuyNum());
-//				odm.setPrice(10.0f);
-//				odm.setMoney(odm.getPrice() * odm.getOrderNum());
-//				odm.setSaveMoney(0.0f);
-//				odm.setOrderUuid(order.getUuid());
-//				
-//				iods.create(odm);
-//				//4:
-//				StoreModel storeModel = iss.getByGoodsUuid(cm.getGoodsUuid());
-//				storeModel.setStoreNum(storeModel.getStoreNum() - odm.getOrderNum());
-//				iss.update(storeModel);
-//				
-//				//5:
-//	//			ics.delete(cm.getUuid());
-//			}	
-//		
-//		long a2 = new Date().getTime();
-//		System.out.println("NONO send one msg time==="+(a2-a));
+			//消息的消费者来完成如下功能
+			CartQueryModel cqm = new CartQueryModel();
+			cqm.getPage().setPageShow(1000);
+			cqm.setCustomerUuid(customerUuid);
+
+			Page<CartModel> page = ics.getByConditionPage(cqm);
+			//2:
+			float totalMoney = 0.0f;
+			for(CartModel cm : page.getResult()){
+				totalMoney += 10;
+			}
+
+			OrderModel order = new OrderModel();
+			order.setCustomerUuid(customerUuid);
+			order.setOrderTime(DateFormatHelper.long2str(System.currentTimeMillis()));
+			order.setSaveMoney(0.0F);
+			order.setTotalMoney(totalMoney);
+			order.setState(1);
+
+			create(order);
+
+			OrderQueryModel oqm = new OrderQueryModel();
+			oqm.setOrderTime(order.getOrderTime());
+
+			Page<OrderModel>  orderPage = getByConditionPage(oqm);
+			order = orderPage.getResult().get(0);
+
+			//3:
+			for(CartModel cm : page.getResult()){
+				OrderDetailModel odm = new OrderDetailModel();
+				odm.setGoodsUuid(cm.getGoodsUuid());
+				odm.setOrderNum(cm.getBuyNum());
+				odm.setPrice(10.0f);
+				odm.setMoney(odm.getPrice() * odm.getOrderNum());
+				odm.setSaveMoney(0.0f);
+				odm.setOrderUuid(order.getUuid());
+
+				iods.create(odm);
+				//4:
+				StoreModel storeModel = iss.getByGoodsUuid(cm.getGoodsUuid());
+				storeModel.setStoreNum(storeModel.getStoreNum() - odm.getOrderNum());
+				iss.update(storeModel);
+
+				//5:
+	//			ics.delete(cm.getUuid());
+			}
+
+		long a2 = new Date().getTime();
+		//System.out.println("NONO send one msg time==="+(a2-a));
 	}
 	
 }
